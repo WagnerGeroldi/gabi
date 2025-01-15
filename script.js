c266dae5de39db6466d579baded02f2402655194
wagnerGeroldi <wagnergeroldi@gmail.com>
2025-01-15 16:11:40 -0300

    v1

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("oi")
+let item1 = document.getElementById("item1")
+
+
+function aparecer() {
+    btn.classList.add("none")
+    item1.classList.remove("none")
+
+}
