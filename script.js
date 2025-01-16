b2add4b15347fccffad3f40a61f5a410fade49c2
wagnerGeroldi <wagnergeroldi@gmail.com>
2025-01-15 21:48:02 -0300

    v7

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let btnsc01 = document.querySelector(".btn-sc-01")
-let sc02 = document.getElementById("sc-02")
+let btnsc02 = document.querySelector(".btn-sc-02")
+let sc02 = document.querySelector(".sc-02")
 
 
 function handleSc01() {
     btnsc01.classList.add("none")
     sc02.classList.remove("none")
+
+}
+
+function handleSc02() {
+    btnsc02.classList.add("none")
+    // sc02.classList.remove("none")
+    alert("TESTE")
 
 }
