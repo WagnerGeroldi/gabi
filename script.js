368abcb87deadeedf9b1517cc48bc15745e90fe7
wagnerGeroldi <wagnergeroldi@gmail.com>
2025-01-15 22:51:15 -0300

    v10

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-let btnsc01 = document.querySelector(".btn-sc-01")
-let btnsc02 = document.querySelector(".btn-sc-02")
-let btnsc03 = document.querySelector(".btn-sc-03")
-let sc02 = document.querySelector(".sc-02")
-let sc03 = document.querySelector(".sc-03")
-let sc04 = document.querySelector(".sc-04")
+let btnsc01 = document.querySelector(".btn-sc-01");
+let btnsc02 = document.querySelector(".btn-sc-02");
+let btnsc03 = document.querySelector(".btn-sc-03");
+let sc02 = document.querySelector(".sc-02");
+let sc03 = document.querySelector(".sc-03");
+let sc04 = document.querySelector(".sc-04");
 
 function handleSc01() {
-    btnsc01.classList.add("none")
-    sc02.classList.remove("none")
-
+  btnsc01.classList.add("none");
+  sc02.classList.remove("none");
 }
 
 function handleSc02() {
-    btnsc02.classList.add("none")
-    sc03.classList.remove("none")
-
+  btnsc02.classList.add("none");
+  sc03.classList.remove("none");
 }
 
 function handleSc03() {
-    btnsc03.classList.add("none")
-    sc04.classList.remove("none")
-
+  btnsc03.classList.add("none");
+  sc04.classList.remove("none");
 }
